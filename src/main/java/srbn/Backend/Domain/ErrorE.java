d4c23cc0a4faf8954f404a06f79e315009f1b48e
SirBonn <byronvasquez21@gmail.com>
2024-09-10 07:40:36 -0600

    update frontend

---
+++ src/main/java/srbn/Backend/Domain/ErrorE.java
@@ -0,0 +1,21 @@
+package srbn.Domain.Errors;
+
+public class ErrorE extends Exception{
+
+    public ErrorE(String message) {
+        super(message);
+    }
+
+    public ErrorE(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ErrorE(Throwable cause) {
+        super(cause);
+    }
+
+    public ErrorE(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
